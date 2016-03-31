25d0956b416794d8142a820299ca100fa52c7302
Thomas Nix <tnix2@uncc.edu>
2016-03-30 21:32:04 -0400

    Re-establishing repository after a flaw with the previous repository. Cleaned up the directories.

---
+++ js/func.js
@@ -0,0 +1,10 @@
+//JSON test data
+/*
+
+
+$(document).ready(function() {
+  var randomNum=Math.floor((Math.random() * 5)+1);
+  console.log(actual_JSON[randomNum].Name);
+  console.log(actual_JSON[randomNum].Quote);
+});
+*/
