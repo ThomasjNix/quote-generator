e19db05483835832d28231da71749ae67ad287da
Thomas Nix <tnix2@uncc.edu>
2016-05-22 20:19:08 -0400

    Fixed some CSS bugs that popped up in transition. Fixed JS to properly select navbar item (problem arose from switching to a single include .php file). Still need to figure out why the JS isn't working properly on contact.php.

--- beta/includes/navbar.php
+++ beta/includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="nav_bar">
     <ul class="nav_list">
-        <li class="nav_li nav_selected"><a href="index.php">Home</a></li>
+        <li class="nav_li"><a href="index.php">Home</a></li>
         <li class="nav_li"><a href="about.php">About</a></li>
         <li class="nav_li"><a href="quotes.php">Quotes</a></li>
         <li class="nav_li"><a href="contact.php">Contact</a></li>
--- beta/js/func.js
+++ beta/js/func.js
@@ -69,8 +69,43 @@ function verifyDisplay(){
   }
 }
 
+//Highlights the current page in the navigation bar by checking the
+//pathname and adding a class within a switch statement as well as
+//removing the class from all other elements (for page changing)
+function highlight_header(){
+  path = window.location.pathname;
+  current_page = window.location.pathname.substr(path.lastIndexOf('/')+1, path.length);
+  current_page = current_page.replace('.php','');
+  console.log(current_page);
+
+  nav_items = document.getElementsByClassName('nav_li');
+  switch(current_page){
+    case('index'):
+      $(nav_items).removeClass('nav_selected');
+      $(nav_items[0]).addClass('nav_selected');
+      break;
+    case('about'):
+      $(nav_items).removeClass('nav_selected');
+      $(nav_items[1]).addClass('nav_selected');
+      break;
+    case('quotes'):
+      $(nav_items).removeClass('nav_selected');
+      $(nav_items[2]).addClass('nav_selected');
+      break;
+    case('contact'):
+      $(nav_items).removeClass('nav_selected');
+      ($nav_items[3]).addClass('nav_selected');
+      break;
+  }
+}
+
 $(document).ready(function() {
+  //Verifies screen display
   verifyDisplay();
+
+  //highlights selected page in navigation bar
+  highlight_header();
+
   //Button action for selected option
   $('button').on("click", function(){
     $.getJSON('json/quotes.json', function(data){
